4bb6b84c8251972d751578945eb9dcdfb0bb2809
Joaquim-Julian <jjuliangalvez03@gmail.com>
2025-04-29 13:55:56 +0200

    numero que indica la cantidad de una cosa en la tienda absolute

--- frontend/src/components/ShopItem.jsx
+++ frontend/src/components/ShopItem.jsx
@@ -4,7 +4,7 @@ function ShopItem({ name, price, image, itemCount }) {
     return (
         <button className="button w-[500px]">
             <span className={`button_top h-[100px] ${(localStorage.getItem('count') >= price) ? "bg-gray-200" : "bg-gray-500"}`}>
-                <div className="text-xl flex flex-row gap-20 text-black mt-[-25px]">
+                <div className="text-xl flex flex-row gap-20 text-black mt-[-25px] relative">
                     <div className='flex flex-row gap-10'>
                         <div className="h-20 w-20 overflow-hidden relative">
                             <img src={`${(localStorage.getItem('count') >= price / 2 || itemCount !== 0) ? image : incognite}`} alt={name} className="object-cover w-full h-full absolute top-0 left-0"/>
@@ -14,7 +14,7 @@ function ShopItem({ name, price, image, itemCount }) {
                             <p>{(localStorage.getItem('count') >= price / 2 || itemCount !== 0) ? price : "???"} timbres</p>
                         </div>
                     </div>
-                    <b className='text-5xl text-gray-800 ml-auto mt-3'>{itemCount}</b>
+                    <b className='text-5xl text-gray-800 right-0 mt-3 absolute'>{itemCount}</b>
                 </div>
             </span>
         </button>
