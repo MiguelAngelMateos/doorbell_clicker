9cf5e28a238677539a49e4279b0724ce9dbd0afc
MiguelAngelMateos <mateosmiguelangel3@gmail.com>
2025-04-29 09:26:10 +0200

    menu de tutorial

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,26 @@
 import { Link } from 'react-router-dom';
+import incognite from '../assets/icons/incognite.png';
 
 function Header({ count, clicksPerSecond }) {
     return (
         <header className="bg-[#202020] flex flex-row py-4 justify-between items-center">
-            <div className="flex flex-col gap-2 text-center ml-68">
-                <h1 className="text-5xl bubble-text">{count.toFixed(0)} timbres</h1>
-                <span className="text-2xl">Timbres por segundo: {clicksPerSecond.toFixed(2)}</span>
+            <div className="flex flex-row gap-2 text-center ml-16">
+                <div className='relative group'>
+                    <img src={incognite} alt="ayuda" className='bg-gray-200 rounded-full p-1 w-[40px] mt-5 cursor-pointer' />
+                    <p className='absolute left-1/2 transform -translate-x-1/2 mt-4 ml-32 w-[300px] p-2 text-sm text-gray-700 bg-white border border-gray-300 rounded-md shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-200 z-10'>
+                        <p className='font-extrabold text-xl'>Bienvenido a Doorbell Clicker!</p>
+                        <br />
+                        <p>Tu objetivo es molestar a todo el vecindario tocando el timbre de su puerta lo más rápido posible!</p>
+                        <br />
+                        <p>Gasta algunos de tus timbres comprando ayudantes en la tienda y desbloquea mejoras permanentes al llegar a ciertos hitos!</p>
+                        <br />
+                        <p>Buena suerte!</p>
+                    </p>
+                </div>
+                <div className='ml-40'>
+                    <h1 className="text-5xl bubble-text">{count.toFixed(0)} timbres</h1>
+                    <span className="text-2xl">Timbres por segundo: {clicksPerSecond.toFixed(2)}</span>
+                </div>
             </div>
             <div className="flex mr-20 gap-18 text-2xl">
                 <Link to="/Leaderboard">Clasificación</Link>
